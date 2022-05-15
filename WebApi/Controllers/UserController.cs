ced4a22a7a139eab2b83fc65afe006348af5f311
gowawe0202 <gowawesales8@gmail.com>
2022-05-15 23:01:28 +0300

    selcuk

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,5 +12,36 @@ namespace WebApi.Controllers
 {
     public class UserController : ApiController
     {
+        public string GetLogin(string username, string password)
+        {
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["webapi"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    con.Open();
+                    {
+                        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("select * from [user] where " +
+                            "(username='" + username + "') and " +
+                            "(password='" + password + "')", con);
+                        DataTable dataTable = new DataTable();
+                        sqlDataAdapter.Fill(dataTable);
+                        if (dataTable.Rows.Count > 0)
+                        {
+                            return "1";
+                        }
+                        else
+                        {
+                            return "0";
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return "0";
+            }
+
+        }
     }
 }
