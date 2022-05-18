797c21891d3942e17da442a9932976eed9e20ae9
selsysoft <67559667+Selsysoft@users.noreply.github.com>
2022-05-18 22:21:21 +0300

    selcuk

--- WebApi/App_Start/WebApiConfig.cs
+++ WebApi/App_Start/WebApiConfig.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-using WebApi.Security;
+
 
 namespace WebApi
 {
@@ -11,10 +11,8 @@ namespace WebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
-            config.MessageHandlers.Add(new APIKeyHandler());
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
